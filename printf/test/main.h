325b262236d6f763700375855212d8d3aebfb221
Wonders <wondersprince1@gmail.com>
2023-08-20 17:28:22 -0700

    Done

---
+++ printf/test/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define UNUSED(x) (void)(x)
+
+
+int print_int(int num);
+int _printf(const char *format, ...);
+int print_str(char *s);
+/* void print_char(char *s); */
+typedef struct
+{
+	/* Struct for specifier handling*/
+	char specifier;
+	void (*handler)(va_list args, int *pchar);
+} FormatHandlerInfo;
+
+void handleChar(va_list args, int *pchar);
+void handleString(va_list args, int *pchar);
+void handleInt(va_list args, int *pchar);
+extern FormatHandlerInfo formatHandlers[];
+int formatLoop(const char *format, va_list args, int *pchar);
+void handleSpecifier(const char **format, va_list args, int *pchar);
+void handleNonSpecifier(const char **format, int *pchar);
+FormatHandlerInfo findHandler(char specifier);
+
+#endif
