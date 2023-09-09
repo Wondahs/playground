bd9a97f7539f721584d3f54c7269457db0bfdceb
Wonders Victor <wondersprince1@gmail.com>
2023-09-09 17:42:27 +0100

    More improvements and fixes

--- shell/shell.c
+++ shell/shell.c
@@ -1,42 +1,51 @@
 #include "main.h"
 
+#define MAX_ARGS 128
 int main(void)
 {
 	char *cmd, *token;
 	char *fullPath;
-	int i = 0;
-	char *args[] = {NULL};
+	char *args[MAX_ARGS];
 	/*char *envp[] = {NULL};*/
 
 	while (1)
 	{
+		int j, i = 0;
+
 		/* Get command */
 		cmd = getPrompt();
-
-		/* Tokenize input */
-		token = _strtok(cmd, " ");
-		while (token != NULL)
-		{
-			args[i++] = token;
-			token = _strtok(NULL, " ");
-		}
-		args[i] = NULL;
-
 		/* Check for exit command */
-		if (_strncmp("exit", args[0], _strlen(args[0]) == 0))
+		if (_strncmp("exit", cmd, _strlen(cmd)) == 0)
 		{
+			printf("exiting\n");
 			free(cmd);
 			break;
 		}
-
+		/* Tokenize input */
+		token = _strtok(cmd, " ");
+		while (token != NULL && i < MAX_ARGS - 1)
+		{
+			args[i++] = _strdup(token);
+			token = _strtok(NULL, " ");
+		}
+		args[i] = NULL;
 		/* Check if command exixts */
 		fullPath = checkPath(args[0]);
 		if (fullPath == NULL)
-			perror("./shell");
+		{
+			write(STDOUT_FILENO, args[0], _strlen(args[0]));
+			perror(">");
+		}
 		else
 		{
 			args[0] = fullPath;
 			execve(args[0], args, NULL);
+		}
+		/* Free allocated memory */
+		for (j = 0; j < i; j++)
+		{
+			free(args[j]);
+			args[j] = NULL;
 		}
 		free(fullPath);
 		free(cmd);
