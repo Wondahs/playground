5f0c01f866b7f44261ad864e0243be82223a3bcf
Wonders Victor <wondersprince1@gmail.com>
2023-09-10 14:17:34 +0100

    Custom printf

--- printf/shared/2-main.c
+++ printf/shared/2-main.c
@@ -1,13 +1,12 @@
+#include "printf.h"
 #include <stdio.h>
-#include <stdarg.h>
-#include "2-main.h"
 
 int main(void)
 {
 	int num = 42;
 	char str[] = "Hello, world!";
 
-	int printed_chars = my_printf("Number: %d\nString: %s\n", num, str);
+	int printed_chars = _printf("Number: %d\nString: %s\n", num, str);
 	printf("Total characters printed: %d\n", printed_chars);
 	return (0);
 }
