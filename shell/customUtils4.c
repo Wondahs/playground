c81566bd5057145e5df3ead974b573aeab6e38b8
Wonders Victor <wondersprince1@gmail.com>
2023-09-11 01:23:38 +0100

    Fixed betty errors

--- shell/customUtils4.c
+++ shell/customUtils4.c
@@ -40,30 +40,30 @@ void call_exec(int tok_count, char *args[], int cmd_count, char *arg)
  *
  *Return: Corrected string
  */
-char *rmv_space(char *cmd) {
-    int i, j = 0;
-    char *cmd_clean;
-    bool prev_space = (cmd[0] == ' ' || cmd[0] == '\t');
+char *rmv_space(char *cmd)
+{
+	int i, j = 0;
+	char *cmd_clean;
+	bool prev_space = (cmd[0] == ' ' || cmd[0] == '\t');
 
+	cmd_clean = malloc(_strlen(cmd) + 1);
 
-    cmd_clean = malloc(_strlen(cmd) + 1);
-
-    for (i = 0; cmd[i] != '\0'; i++) {
-        if (cmd[i] == ' ' || cmd[i] == '\t') {
-            if (!prev_space) {
-                cmd_clean[j++] = cmd[i];
-            }
-            prev_space = true;
-        } else {
-            cmd_clean[j++] = cmd[i];
-            prev_space = false;
-        }
-    }
-    
-    cmd_clean[j] = '\0';
-
-    free(cmd);
-    cmd = cmd_clean;
-    return cmd;
+	for (i = 0; cmd[i] != '\0'; i++)
+	{
+		if (cmd[i] == ' ' || cmd[i] == '\t')
+		{
+			if (!prev_space)
+				cmd_clean[j++] = cmd[i];
+			prev_space = true;
+		}
+		else
+		{
+			cmd_clean[j++] = cmd[i];
+			prev_space = false;
+		}
+	}
+	cmd_clean[j] = '\0';
+	free(cmd);
+	cmd = cmd_clean;
+	return (cmd);
 }
-
