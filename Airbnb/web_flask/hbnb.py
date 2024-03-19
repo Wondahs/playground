1536904de0704f2181df913927bcbef29e4f9178
Wonders Victor <wondersprince1@gmail.com>
2024-03-19 11:08:13 +0100

    Hbnb Practice

--- Airbnb/web_flask/hbnb.py
+++ Airbnb/web_flask/hbnb.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-from os import getenv
+from models.amenity import Amenity
 
 
 app = Flask(__name__)
@@ -14,22 +14,18 @@ def app_teardown(exception):
     '''Closes storage'''
     storage.close()
 
-@app.route("/states_list", strict_slashes=False)
+@app.route("/hbnb_filters", strict_slashes=False)
 def states_list():
     '''Displays states'''
     states_obj = list(storage.all(State).values())
     sorted_states = sorted(states_obj, key=lambda x: x.name)
-    return render_template("states.html", state_list=sorted_states)
-
-@app.route("/states/<id>", strict_slashes=False)
-def state_id(id):
-    '''Retrieves a state'''
-    states_obj = list(storage.all(State).values())
-    sorted_states = sorted(states_obj, key=lambda x: x.name)
-    return render_template("states.html", state_list=sorted_states, id=id)
+    amenities_list = list(storage.all(Amenity).values())
+    sorted_amenities = sorted(amenities_list, key=lambda x: x.name)
+    return render_template("hbnb_filters.html",
+                           state_list=sorted_states,
+                           amenities=sorted_amenities)
 
 if __name__ == "__main__":
     host = "0.0.0.0"
     port = 5000
     app.run(host=host, port=port, debug=True)
-
