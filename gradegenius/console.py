60682a79160848b57e768a57746980152079cbf5
Wonders Victor <wondersprince1@gmail.com>
2024-03-19 14:10:37 +0100

    Working

--- gradegenius/console.py
+++ gradegenius/console.py
@@ -7,7 +7,7 @@ def create_user():
 	'''Creates User for demo testing'''
 	new_user = User()
 	name = input("Enter Name: ")
-	new_user.name = name
+	new_user.name = name.title()
 	username = input("Enter Username: ")
 	new_user.username = username
 	school = input("Enter School: ")
@@ -15,9 +15,9 @@ def create_user():
 	print("Account successfully created")
 	return new_user
 
-print('*' * 30)
+print('*' * 40)
 print("\tWelcome to GradeGenius")
 create_prompt = input("Do you wanna create an account? Y / N: ")
-if create_prompt.upper == 'Y':
+if create_prompt.upper() == 'Y':
 	new_user = create_user()
 	print("Your name is {}.\nYour username is {}.\nYour school is {}".format(new_user.name, new_user.username, new_user.school))
---
+++ gradegenius/console2.py
@@ -0,0 +1,5 @@
+#!/bin/python3
+'''Console program for grade genius'''
+from calculator.grade_cal import grade_cal
+from models.user import User
+from cmd
