2e78b96055ef487936aa3222ae42273c58a21453
Wonders Victor <wondersprince1@gmail.com>
2024-04-16 06:22:26 +0100

    Fixed Nav Bar

--- ecommerce-product-page-main/scripts/script.js
+++ ecommerce-product-page-main/scripts/script.js
@@ -1,23 +1,25 @@
 let menuButton = document.getElementById("menu-button");
 let navBar = document.getElementById("nav-bar");
 
+
 menuButton.addEventListener('click', () => {
-	let navBar = document.getElementById("nav-bar");
 	if (menuButton.state === "closed") {
 		navBar.style.display = "flex";
-		menuButton.src = "../images/icon-close.svg";
-		menuButton.state === "opened";
+		menuButton.src = "./images/icon-close.svg";
+		menuButton.state = "opened";
 	} else {
 		navBar.style.display = "none";
-		menuButton.src = "../images/icon-menu.svg";
+		menuButton.src = "./images/icon-menu.svg";
 		menuButton.state = "closed";
 	}
+	console.log("clicked me");
 });
 
 document.addEventListener('click', event => {
-	if (event.target !== menuButton && event.target !== navBar) {
+	if (event.target !== menuButton && !navBar.contains(event.target) && (window.innerWidth < 768)) {
 		navBar.style.display = "none";
 		menuButton.state = "closed";
-		menuButton.src = "../images/icon-menu.svg";
+		menuButton.src = "./images/icon-menu.svg";
 	}
 });
+
