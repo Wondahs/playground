0cd04b051cc6548fbc9a6369cd21cf221834f0e6
Wonders Victor <wondersprince1@gmail.com>
2024-04-16 22:53:55 +0100

    working

--- ecommerce-product-page-main/scripts/popup.js
+++ ecommerce-product-page-main/scripts/popup.js
@@ -1,4 +1,3 @@
-let closePopup = document.querySelector("#popup #close-popup");
 let popup = document.getElementById("popup");
 let left = document.querySelector(".left");
 
@@ -8,6 +7,7 @@ popup.innerHTML = `<img id="close-popup" src="./images/icon-close.svg" alt="">
 let popupThumbnails = document.querySelectorAll("#popup .left .thumbnails img")
 let previewImg = document.querySelectorAll(".preview #preview-image");
 let normalThumbnails = document.querySelectorAll("main .left .thumbnails img");
+let closePopup = document.querySelector("#popup #close-popup");
 
 
 closePopup.addEventListener('click', () => {
@@ -31,6 +31,6 @@ function thumbnailsPreview (thumbnails, preview) {
 }
 
 previewImg[1].addEventListener('click', () => {
-	popup.querySelector("#preview-image").scr = this.src;
+	popup.querySelector("#preview-image").src = this.src;
 	popup.style.display = "flex";
 })
