57d563fbfd3c4a631d37eac05f86f1d5af84e953
Wonders Victor <wondersprince1@gmail.com>
2024-04-16 10:01:54 +0000

    Added cart and other features

--- ecommerce-product-page-main/scripts/script.js
+++ ecommerce-product-page-main/scripts/script.js
@@ -1,6 +1,14 @@
 let menuButton = document.getElementById("menu-button");
 let navBar = document.getElementById("nav-bar");
+let cartButton = document.getElementById("cart-icon");
+let cartDiv = document.getElementById("cart");
+let plusButton = document.getElementById("plus-button");
+let minusButton = document.getElementById("minus-button");
+let amount = document.getElementById("amount");
+let itemCount = 0;
 
+cartDiv.state = "closed";
+amount.textContent = 0;
 
 menuButton.addEventListener('click', () => {
 	if (menuButton.state === "closed") {
@@ -23,3 +31,30 @@ document.addEventListener('click', event => {
 	}
 });
 
+cartButton.addEventListener('click', () => {
+	if (cartDiv.state === "closed") {
+		cartDiv.style.display = "flex";
+		cartDiv.state = "opened"
+	} else {
+		cartDiv.style.display = "none";
+		cartDiv.state = "closed";
+	}
+});
+
+document.addEventListener('click', event => {
+	if (event.target !== cartButton && !cartDiv.contains(event.target)) {
+		cartDiv.style.display = "none";
+		cartDiv.state = "closed";
+	}
+});
+
+plusButton.addEventListener('click', () => {
+	amount.textContent = `${itemCount++}`;
+	console.log("Plus 1");
+});
+
+minusButton.addEventListener('click', () => {
+	if (itemCount > 0) {
+		amount.textContent = `${--itemCount}`;
+	}
+});
