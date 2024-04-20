82c0ee045ce0538fe835f1351cc393bbafcda57c
Wonders Victor <wondersprince1@gmail.com>
2024-04-20 18:22:33 +0100

    Working on JS

---
+++ multi-step-form-main/assets/scripts/steps.js
@@ -0,0 +1,25 @@
+let steps = document.querySelectorAll('[class^="step"]');
+
+let next = window.innerWidth < 768 ? document.querySelector('footer #next') : document.querySelector('.progress .next');
+let back = window.innerWidth < 768 ? document.querySelector('footer #back') : document.querySelector('.progress .back');
+
+
+window.addEventListener('resize', () => {
+	next = window.innerWidth < 768 ? document.querySelector('footer #next') : document.querySelector('.progress .next');
+	back = window.innerWidth < 768 ? document.querySelector('footer #back') : document.querySelector('.progress .back');
+
+	console.log(next ? next.textContent : "not found");
+})
+for (let step of steps) {
+	step.style.display = 'none';
+}
+
+let currentStepCount = 1;
+let currentStep = document.querySelector(`.step-${currentStepCount}`);
+
+if (currentStepCount === 1) {
+	console.log("Step 1")
+	next.style.display = "none";
+}
+
+currentStep.style.display = 'flex';
