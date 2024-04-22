c449b0dc722827d698cc5f522f9d6dc6831f8f2f
Wonders Victor <wondersprince1@gmail.com>
2024-04-22 19:21:39 +0100

    working

--- multi-step-form-main/assets/scripts/finishing.js
+++ multi-step-form-main/assets/scripts/finishing.js
@@ -1,5 +1,7 @@
 let plan = document.querySelector(".step-4 .finishing .plan h3");
-let addOns = document.querySelectorAll(".step-4 .finishing p");
+let addOns = document.querySelectorAll(".step-4 .finishing .finishing-details p");
 
 console.log(plan.textContent);
 console.log(addOns);
+
+
--- multi-step-form-main/assets/scripts/steps.js
+++ multi-step-form-main/assets/scripts/steps.js
@@ -1,35 +1,63 @@
 let steps = Array.from(document.querySelectorAll('[class^="step-"]')).filter(element => /\d+/.test(element.className.split('-')[1]));
 
-let next = window.innerWidth < 768 ? document.querySelector('footer #next') : document.querySelector('.progress .next');
-let back = window.innerWidth < 768 ? document.querySelector('footer #back') : document.querySelector('.progress .back');
+let next = window.innerWidth < 768 ? document.querySelector('footer #next') : document.querySelector('.progress .next button');
+let back = window.innerWidth < 768 ? document.querySelector('footer #back') : document.querySelector('.progress .back button');
 let currentStepCount = 1;
 let currentStep = document.querySelector(`.step-${currentStepCount}`);
 let footer = document.getElementById("footer");
+let stepOneInputs = document.querySelectorAll('.step-1 input');
+let stepTwoInputs = document.querySelectorAll('.step-2 input');
 
 back.style.display = "none";
+next.classList.add("dim-button");
+next.disabled = true;
+
+function planClicked () {
+	let plans = Array.from(stepTwoInputs);
+	let planSelected = plans.some(element => element.checked);
+	if (currentStepCount === 2) {
+		if (planSelected) {
+			next.classList.remove("dim-button");
+			next.disabled = false;
+		} else {
+			next.classList.add("dim-button");
+			next.disabled = true;
+		}
+	}
+}
 
 window.addEventListener('resize', () => {
 	console.log("Resized");
-	next = window.innerWidth < 768 ? document.querySelector('footer #next') : document.querySelector('.progress .next');
-	back = window.innerWidth < 768 ? document.querySelector('footer #back') : document.querySelector('.progress .back');
+	next = window.innerWidth < 768 ? document.querySelector('footer #next') : document.querySelector('.progress .next button');
+	back = window.innerWidth < 768 ? document.querySelector('footer #back') : document.querySelector('.progress .back button');
 	footer.style.display = window.innerWidth < 768 && currentStepCount < 5 ?"flex" : "none";
 	hideSteps();
 	console.log(currentStepCount);
-	currentStep = document.querySelector(`.step-${currentStepCount}`);
-	currentStep.style.display = 'flex';
-
-	back.style.display = currentStepCount > 1 ? "flex" : "none";
 	next.addEventListener('click', nextStep);
 	back.addEventListener('click', prevStep);
+	currentStep = document.querySelector(`.step-${currentStepCount}`);
+	currentStep.style.display = 'flex';
+	allFilled();
+
+	back.style.display = currentStepCount > 1 ? "flex" : "none";
 });
 
-hideSteps();
+function allFilled () {
+	let inputs = Array.from(stepOneInputs);
+	let filled = inputs.every(element => element.value !== '');
 
-function hideSteps () {
-	for (let step of steps) {
-		step.style.display = 'none';
+	if (currentStepCount === 1) {
+		if (filled) {
+			next.classList.remove("dim-button");
+			next.disabled = false;
+		} else {
+			next.classList.add("dim-button");
+			next.disabled = true;
+		}
 	}
 }
+
+hideSteps();
 
 let stepButton = document.getElementById(`button-${currentStepCount}`);
 stepButton.style.backgroundColor = "hsl(228, 100%, 84%)";
@@ -40,6 +68,19 @@ currentStep.style.display = 'flex';
 next.addEventListener('click', nextStep);
 back.addEventListener('click', prevStep);
 
+stepOneInputs.forEach(element => {
+	element.addEventListener('input', allFilled);
+});
+
+stepTwoInputs.forEach(element => {
+	element.addEventListener('change', planClicked);
+});
+
+function hideSteps () {
+	for (let step of steps) {
+		step.style.display = 'none';
+	}
+}
 
 function prevStep () {
 	if (currentStepCount > 1) {
@@ -66,11 +107,25 @@ function nextStep () {
 		stepButton.style.backgroundColor = "hsl(228, 100%, 84%)";
 		console.log(currentStepCount);
 		stepButton.style.color = "hsl(213, 96%, 18%)";
+		back.style.display = (currentStepCount < 2 || currentStepCount > 4) ? "none" : "flex";
 	}
-	back.style.display = (currentStepCount < 2 || currentStepCount > 4) ? "none" : "flex";
 	if (window.innerWidth < 768) {
 		footer.style.display = (currentStepCount < 2 || currentStepCount > 4) ? "none" : "flex";
 	} else {
 		next.style.display = (currentStepCount < 2 || currentStepCount > 4) ? "none" : "flex";
+	}
+
+	if (currentStepCount === 2) {
+		planClicked();
+	}
+
+	if (currentStepCount === 3) {
+		let selectedPlan = document.querySelector('input[name="plan"]:checked');
+		console.log("Selected plan is ", selectedPlan.value);
+	}
+
+	if (currentStepCount === 4) {
+		let selectedAddOns = Array.from(document.querySelectorAll('input[type="checkbox"][name="add-ons"] + .label')).filter(element => element.checked);
+		console.log(selectedAddOns);
 	}
 }
