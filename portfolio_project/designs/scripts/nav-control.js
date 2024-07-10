fd304e2d3ac83062a7a5f34a7954cece8318d790
Wonders Victor <wondersprince1@gmail.com>
2024-07-10 22:10:50 +0100

    Added js for nav menu

---
+++ portfolio_project/designs/scripts/nav-control.js
@@ -0,0 +1,33 @@
+const navButton = document.getElementById('nav-button');
+const navBar = document.getElementById('nav-bar');
+const filter = document.getElementById('filter');
+
+navButton.setAttribute('open', 'false');
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 900) {
+        navBar.style.display = 'flex';
+        navButton.style.display = 'none';
+        document.body.style.overflow = 'visible';
+        navButton.setAttribute('open', 'false');
+        if (filter.classList.contains('filter')) filter.classList.remove('filter');
+    } else {
+        navBar.style.display = 'none';
+        navButton.style.display = 'flex';
+        document.body.style.overflow = 'visible';
+    }
+});
+
+navButton.addEventListener('click', () => {
+    if (navButton.getAttribute('open') === 'false') {
+        navBar.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+        navButton.setAttribute('open', 'true');
+        filter.classList.add('filter');
+    } else {
+        navBar.style.display = 'none';
+        document.body.style.overflow = 'visible';
+        navButton.setAttribute('open', 'false');
+        filter.classList.remove('filter');
+    }
+});
