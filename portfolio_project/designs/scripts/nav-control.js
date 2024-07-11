f0ef8185c4ee682a0160c6c13c11467bd448f5bc
Wonders Victor <wondersprince1@gmail.com>
2024-07-11 14:15:02 +0000

    Working

--- portfolio_project/designs/scripts/nav-control.js
+++ portfolio_project/designs/scripts/nav-control.js
@@ -5,29 +5,30 @@ const filter = document.getElementById('filter');
 navButton.setAttribute('open', 'false');
 
 window.addEventListener('resize', () => {
-    if (window.innerWidth > 900) {
-        navBar.style.display = 'flex';
-        navButton.style.display = 'none';
-        document.body.style.overflow = 'visible';
-        navButton.setAttribute('open', 'false');
-        if (filter.classList.contains('filter')) filter.classList.remove('filter');
-    } else {
-        navBar.style.display = 'none';
-        navButton.style.display = 'flex';
-        document.body.style.overflow = 'visible';
-    }
+  if (window.innerWidth > 900) {
+    navBar.style.display = 'flex';
+    navButton.style.display = 'none';
+    document.body.style.overflow = 'visible';
+    navButton.setAttribute('open', 'false');
+    if (filter.classList.contains('filter')) filter.classList.remove('filter');
+  } else {
+    navBar.style.display = 'none';
+    navButton.style.display = 'flex';
+    document.body.style.overflow = 'visible';
+  }
 });
 
 navButton.addEventListener('click', () => {
-    if (navButton.getAttribute('open') === 'false') {
-        navBar.style.display = 'flex';
-        document.body.style.overflow = 'hidden';
-        navButton.setAttribute('open', 'true');
-        filter.classList.add('filter');
-    } else {
-        navBar.style.display = 'none';
-        document.body.style.overflow = 'visible';
-        navButton.setAttribute('open', 'false');
-        filter.classList.remove('filter');
-    }
+  if (navButton.getAttribute('open') === 'false') {
+    navBar.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+    navButton.setAttribute('open', 'true');
+    navButton.style.src = '../images/close-nav-icon.svg'
+    filter.classList.add('filter');
+  } else {
+    navBar.style.display = 'none';
+    document.body.style.overflow = 'visible';
+    navButton.setAttribute('open', 'false');
+    filter.classList.remove('filter');
+  }
 });
