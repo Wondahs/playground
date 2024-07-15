60abbb21f2093dd10972053affd1f8a2300446cc
Wonders Victor <wondersprince1@gmail.com>
2024-07-15 21:54:59 +0100

    Working.

--- react_projects/dojo-blog/src/BlogList.js
+++ react_projects/dojo-blog/src/BlogList.js
@@ -1,31 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const BlogList = ({ blogs, title, setData }) => {
 
     const [author, setAuthor] = useState('');
+    const [displayBlog, setDisplayBlog] = useState(blogs)
+
+    useEffect(() => {
+        const filtered = blogs.filter((blog) => blog.author.toLowerCase().includes(author.toLowerCase()));
+        setDisplayBlog(filtered);
+    }, [author, blogs, setData]);
 
     const handleSearch = (e) => {
-        e.preventDefault();
-        const filtered = blogs.filter((blog) => blog.author === author);
-        setData(filtered);
+        setAuthor(e.target.value);
     }
 
     return (
         <div className="blog-list">
-            <form onSubmit={handleSearch}>
-                <label>Search By Author</label>
+            <form onSubmit={e => e.preventDefault()}>
                 <input
                     type="text"
                     name="author"
                     value={author}
-                    onChange={(e) => setAuthor(e.target.value)}
+                    onChange={handleSearch}
                     placeholder="Enter author name"
                 />
-                <button>Search</button>
             </form>
-            <h1>{blogs.length > 0 ? title : ""}</h1>
-            {blogs.map((blog) => (
+            <h1>{author === '' ? title : `Blogs by ${author}`}</h1>
+            {displayBlog.length === 0 ? <p>`No results found for ${author}`</p> :
+            displayBlog.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <Link to={`/blogs/${blog.id}`}>
                         <h2>{blog.title}</h2>
